5909c770a5d0b8d6f5820522390e056b74e72e80
Asif Mostofa Sazid <asif83sazid@gmail.com>
2025-06-20 00:46:49 +0600

    admin and user login separated

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,4 +5,15 @@
         </div>
     </x-slot>
 
+    <div class="py-12">
+        <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 dark:text-gray-100">
+                        {{ __("Welcome, Mr. ". Auth::user()->name ."! You're logged in and the floor is your!") }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-sb-admin-master>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,10 +6,12 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+        <div class="flex items-center justify-between px-4 py-4 border-b lg:py-6 dark:border-primary-darker">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 dark:text-gray-100">
+                        {{ __("Welcome, ". Auth::user()->name ."! You're logged in!") }}
+                    </div>
                 </div>
             </div>
         </div>
